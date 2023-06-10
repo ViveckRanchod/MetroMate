5ab225605fbd5cc401cb1fc575818b1d67f385a5
Musa <musamichelledube@gmail.com>
2023-06-10 17:32:22 +0200

    added styling

--- app/src/main/java/com/example/metromate01/Driver.java
+++ app/src/main/java/com/example/metromate01/Driver.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class driver_registration_page extends AppCompatActivity {
+public class Driver extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
