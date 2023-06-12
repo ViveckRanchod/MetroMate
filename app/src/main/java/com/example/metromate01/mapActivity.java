798851efc11680ca6964f4d9856035954f496871
Viveck Ranchod <69462420+ViveckRanchod@users.noreply.github.com>
2023-06-12 19:04:44 +0200

    Updated activityMap.java

--- app/src/main/java/com/example/metromate01/mapActivity.java
+++ app/src/main/java/com/example/metromate01/mapActivity.java
@@ -1,4 +1,20 @@
 package com.example.metromate01;
 
-public class mapActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class mapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps); // Replace with your layout file name
+
+        // Retrieve the map container and add the SupportMapFragment
+        SupportMapFragment mapFragment = new SupportMapFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.mapContainer, mapFragment)
+                .commit();
+    }
 }
