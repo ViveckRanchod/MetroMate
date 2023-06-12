c01b69faf93648f142d1ff915fa5b78baf43d7af
Viveck Ranchod <69462420+ViveckRanchod@users.noreply.github.com>
2023-06-12 21:33:25 +0200

    Created the Bottom sheet and Map page

--- app/src/main/java/com/example/metromate01/MainActivity.java
+++ app/src/main/java/com/example/metromate01/MainActivity.java
@@ -1,28 +1,32 @@
 package com.example.metromate01;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.example.metromate01.databinding.ActivityMainBinding;
-
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+
+    private BottomSheetBehavior<View> bottomSheetBehavior;
 
 
 
@@ -30,16 +34,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+// Viveck work for bottom sheet
 
+        setContentView(R.layout.activity_main);
+
+// Inflate the bottom sheet layout
+// Inflate the bottom sheet layout
+        View bottomSheetView = LayoutInflater.from(this).inflate(R.layout.maps_routes_drawer, null);
+
+// Find the bottom sheet container
+        CoordinatorLayout bottomSheetContainer = findViewById(R.id.bottomSheetContainer);
+
+// Set up BottomSheetBehavior
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetView);
+        bottomSheetBehavior.setPeekHeight(200); // Set the initial peek height of the bottom sheet
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED); // Set the initial state of the bottom sheet
+
+// Add the bottom sheet to the container
+        bottomSheetContainer.addView(bottomSheetView);
 
 //        binding.fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
