d5812282c22bc5292ffdd9867f58bc71685f250b
Viveck Ranchod <69462420+ViveckRanchod@users.noreply.github.com>
2023-06-14 22:05:00 +0200

    worked on map

--- app/src/main/java/com/example/metromate01/mapActivity.java
+++ app/src/main/java/com/example/metromate01/mapActivity.java
@@ -1,20 +1,37 @@
 package com.example.metromate01;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.maps.SupportMapFragment;
 
-public class mapActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class mapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap myMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps); // Replace with your layout file name
+        setContentView(R.layout.activity_maps); //Where the map is being placed
 
         // Retrieve the map container and add the SupportMapFragment
-        SupportMapFragment mapFragment = new SupportMapFragment();
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.mapContainer, mapFragment)
-                .commit();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        myMap = googleMap;
+        LatLng jhb = new LatLng(-34,151);
+        myMap.addMarker(new MarkerOptions().position(jhb).title("JHB"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(jhb));
+
     }
 }
