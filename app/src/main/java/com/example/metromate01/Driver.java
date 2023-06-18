58c01730889ce1f707a5fbb859291aebf652bed1
Musa <musamichelledube@gmail.com>
2023-06-18 20:34:07 +0200

    added methods to schedule class, updated driver class methods, updated schedule activities

--- app/src/main/java/com/example/metromate01/Driver.java
+++ app/src/main/java/com/example/metromate01/Driver.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -28,46 +29,40 @@ public class Driver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_registration_page);
-
-
-    //assign variables to xml elements and convert to string:
+    //assign variables to xml elements:
         signUp = findViewById(R.id.btnSignUp);
-
         name = findViewById(R.id.edtName);
-        String st_name = name.getText().toString();
-
         lastname= findViewById(R.id.edtLastName);
-        String st_lastname = lastname.getText().toString();
-
         badgeID=  findViewById(R.id.edtBadgeId);
-        String st_badgeID = badgeID.getText().toString();
-
         email=  findViewById(R.id.edtEmail);
-        String st_email = email.getText().toString();
-
         password=  findViewById(R.id.edtPassword);
-        String st_password = password.getText().toString();
-
         dob=  findViewById(R.id.edtDateOfBirth);
-        String st_dob = dob.getText().toString();
-
 
         //send user input to dbase:
         Database db = new Database();
         //check input conditions:
-        if(!st_name.isEmpty() && !st_lastname.isEmpty() && !st_email.isEmpty() &&!st_password.isEmpty()
-                && st_password.length()>=8 && !st_dob.isEmpty()&& st_badgeID.isEmpty()){
+
         signUp.setOnClickListener(sendView -> {
-            //pass path we want to send data to:
-            db.setPath(path);
+            //get current user inputs:
+            String st_name = name.getText().toString();
+            String st_lastname = lastname.getText().toString();
+            String st_email = email.getText().toString();
+            String st_badgeID = badgeID.getText().toString();
+            String st_password = password.getText().toString();
+            String st_dob = dob.getText().toString();
+
             //set refValues in the same order of the parameters set in signUpToDatabase in getDatabase:
             //refer to Database to see purpose of tagNumber= null
-            db.signUpToDatabase(st_name, st_lastname, st_email, st_password, st_dob, st_badgeID,
+            if(!st_name.isEmpty() && !st_lastname.isEmpty() && !st_email.isEmpty() &&!st_password.isEmpty()
+                    && st_password.length()>=8 && !st_dob.isEmpty()&& st_badgeID.isEmpty()){
+                //pass path we want to send data to:
+                db.setPath(path);
+                db.signUpToDatabase(st_name, st_lastname, st_email, st_password, st_dob, st_badgeID,
                     null, path);
+            }else {
+                Toast.makeText(Driver.this, "Please ensure are fields are filled & password meets specified criteria", Toast.LENGTH_SHORT).show();
+            }
         });
-        }else {
-
-        }
     }
 }
 
--- app/src/main/java/com/example/metromate01/Schedule.java
+++ app/src/main/java/com/example/metromate01/Schedule.java
@@ -1,18 +1,51 @@
 package com.example.metromate01;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Schedule extends AppCompatActivity {
+public class Schedule extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner schedule_droplist;
+    ImageView schedule_img = findViewById(R.id.schedule_img);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_page);
+        //initialise spinner and adapter:
+        schedule_droplist = findViewById(R.id.schedule_droplist);
+        ArrayAdapter<CharSequence> adaptArr = ArrayAdapter.createFromResource(Schedule.this, R.array.areas_array, android.R.layout.simple_spinner_item);
+        adaptArr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        schedule_droplist.setAdapter(adaptArr);
+        schedule_droplist.setSelection(0);
+        schedule_droplist.setOnItemSelectedListener(this);
+    }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        int schedule_selected_id = schedule_droplist.getSelectedItemPosition();
+        switch(schedule_selected_id){
+            case 0:
+                    schedule_img.setImageResource(R.drawable.bus_background);
+                 case 1:
+                         schedule_img.setImageResource(R.drawable.oakdene_schedule);
+                         break;
+                        case 2:
+                               schedule_img.setImageResource(R.drawable.bedford_schedule);
+                               break;
+                               default:
+                                       Toast.makeText(Schedule.this, "Select an schedule to view from the list", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
