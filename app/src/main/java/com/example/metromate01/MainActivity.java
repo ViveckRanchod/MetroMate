d5947ef7d70fe3c160936e3b536b411af7e5bf5a
Viveck Ranchod <69462420+ViveckRanchod@users.noreply.github.com>
2023-06-18 22:08:03 +0200

    Fixed nav bar

--- app/src/main/java/com/example/metromate01/MainActivity.java
+++ app/src/main/java/com/example/metromate01/MainActivity.java
@@ -31,97 +31,51 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-public class MainActivity extends AppCompatActivity
-        implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    // Declare the fragments
-    private FirstFragment firstFragment;
-    private SecondFragment secondFragment;
-    private ThirdFragment thirdFragment;
-    private FourthFragment fourthFragment;
-
-    private LandingFragment landingFragment;
-
-    private BottomSheetBehavior<View> bottomSheetBehavior;
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        landingFragment = new LandingFragment();
-
-        // Initialize the fragments
-        firstFragment = new FirstFragment();
-        secondFragment = new SecondFragment();
-        thirdFragment = new ThirdFragment();
-        fourthFragment = new FourthFragment();
-
-        // Set the initial fragment
-        setInitialFragment();
-
-        // Set the listener for bottom navigation
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-        bottomNavigationView.inflateMenu(R.menu.bottom_nav_menu);
-
-        // Print the maximum item count
-        int maxItemCount = bottomNavigationView.getMaxItemCount();
-        System.out.println("Maximum item count: " + maxItemCount);
-
-        // Set up the bottom sheet
-        View bottomSheet = findViewById(R.id.coordinatorLayout);
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-    }
-
-    private void setInitialFragment() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.flFragment, landingFragment);
-        fragmentTransaction.commit();
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Fragment selectedFragment = null;
-
-        switch (item.getItemId()) {
-            case R.id.homePage:
-                selectedFragment = firstFragment;
-                break;
-
-            case R.id.schedulesPage:
-                selectedFragment = secondFragment;
-                break;
-
-            case R.id.mapsRoutes:
-                selectedFragment = thirdFragment;
-                break;
-
-            case R.id.myTagID:
-                selectedFragment = fourthFragment;
-                break;
-        }
-
-        if (selectedFragment != null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.flFragment, selectedFragment);
-            fragmentTransaction.commit();
-        }
-
-        return true;
-    }
-
-    // Method to show the bottom sheet
-    private void showBottomSheet() {
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-    }
-
-    // Method to hide the bottom sheet
-    private void hideBottomSheet() {
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.navigation_item1:
+                                // Open MainFragment
+                                getSupportFragmentManager().beginTransaction()
+                                        .replace(R.id.flFragment, new MainFragment())
+                                        .commit();
+                                return true;
+                            case R.id.navigation_item2:
+                                // Open SchedulePageFragment
+                                getSupportFragmentManager().beginTransaction()
+                                        .replace(R.id.flFragment, new SchedulePageFragment())
+                                        .commit();
+                                return true;
+                            case R.id.navigation_item3:
+                                // Open MapsFragment
+                                getSupportFragmentManager().beginTransaction()
+                                        .replace(R.id.flFragment, new MapsFragment())
+                                        .commit();
+                                return true;
+                            case R.id.navigation_item4:
+                                // Open MyTagPageFragment
+                                getSupportFragmentManager().beginTransaction()
+                                        .replace(R.id.flFragment, new MyTagPageFragment())
+                                        .commit();
+                                return true;
+                        }
+                        return false;
+                    }
+                });
     }
 }
+
+
+
