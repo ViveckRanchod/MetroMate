bfaceb56148b2f8fb94d524267834d317e663d05
Viveck Ranchod <69462420+ViveckRanchod@users.noreply.github.com>
2023-06-18 23:31:22 +0200

    Opens on landing page and buttons direct to sign up page

--- app/src/main/java/com/example/metromate01/Commuter.java
+++ app/src/main/java/com/example/metromate01/Commuter.java
@@ -1,4 +1,17 @@
 package com.example.metromate01;
 
-public class Commuter {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Commuter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.commuter_sign_up);
+
+        // Additional setup or logic for the activity
+        // ...
+    }
 }
--- app/src/main/java/com/example/metromate01/MainActivity.java
+++ app/src/main/java/com/example/metromate01/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.metromate01;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
@@ -23,6 +24,7 @@ import android.view.MenuItem;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,48 +35,73 @@ import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
     private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.landing_page);
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.navigation_item1:
-                                // Open MainFragment
-                                getSupportFragmentManager().beginTransaction()
-                                        .replace(R.id.flFragment, new MainFragment())
-                                        .commit();
-                                return true;
-                            case R.id.navigation_item2:
-                                // Open SchedulePageFragment
-                                getSupportFragmentManager().beginTransaction()
-                                        .replace(R.id.flFragment, new SchedulePageFragment())
-                                        .commit();
-                                return true;
-                            case R.id.navigation_item3:
-                                // Open MapsFragment
-                                getSupportFragmentManager().beginTransaction()
-                                        .replace(R.id.flFragment, new MapsFragment())
-                                        .commit();
-                                return true;
-                            case R.id.navigation_item4:
-                                // Open MyTagPageFragment
-                                getSupportFragmentManager().beginTransaction()
-                                        .replace(R.id.flFragment, new MyTagPageFragment())
-                                        .commit();
-                                return true;
-                        }
-                        return false;
-                    }
-            });
+        // Find the buttons in the landing page
+        Button signUpButton1 = findViewById(R.id.driverButton);
+        Button signUpButton2 = findViewById(R.id.commuterButton);
+
+        // Set click listeners for the sign-up buttons
+        signUpButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the first sign-up activity
+                Intent intent = new Intent(MainActivity.this, Driver.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the second sign-up activity
+                Intent intent = new Intent(MainActivity.this, Commuter.class);
+                startActivity(intent);
+            }
+        });
+
+//        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+//        bottomNavigationView.setOnNavigationItemSelectedListener(
+//                new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                    @Override
+//                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                        switch (item.getItemId()) {
+//                            case R.id.navigation_item1:
+//                                // Open MainFragment
+//                                getSupportFragmentManager().beginTransaction()
+//                                        .replace(R.id.flFragment, new MainFragment())
+//                                        .commit();
+//                                return true;
+//                            case R.id.navigation_item2:
+//                                // Open SchedulePageFragment
+//                                getSupportFragmentManager().beginTransaction()
+//                                        .replace(R.id.flFragment, new SchedulePageFragment())
+//                                        .commit();
+//                                return true;
+//                            case R.id.navigation_item3:
+//                                // Open MapsFragment
+//                                getSupportFragmentManager().beginTransaction()
+//                                        .replace(R.id.flFragment, new MapsFragment())
+//                                        .commit();
+//                                return true;
+//                            case R.id.navigation_item4:
+//                                // Open MyTagPageFragment
+//                                getSupportFragmentManager().beginTransaction()
+//                                        .replace(R.id.flFragment, new MyTagPageFragment())
+//                                        .commit();
+//                                return true;
+//                        }
+//                        return false;
+//                    }
+//                });
     }
 }
+
 
 
