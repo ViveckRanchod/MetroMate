fdb7c726d1a4fbc1580aa614124750cfb8e540dc
Juhi Kira <106545129+JKiraX@users.noreply.github.com>
2023-06-20 20:37:50 +0200

    Fixed and renamed fragment and view model classes.

--- app/src/main/java/com/example/metromate01/MainActivity.java
+++ app/src/main/java/com/example/metromate01/MainActivity.java
@@ -1,87 +1,40 @@
 package com.example.metromate01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import androidx.appcompat.widget.SearchView;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+import androidx.appcompat.widget.Toolbar;
 
-    private GoogleMap myMap;
-    private SearchView searchView;
-    private List<BusStop> busStops;
+import com.example.metromate01.databinding.ActivityMainBinding;
 
-    @SuppressLint("MissingInflatedId")
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
 
-        // Initializes the map
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map_fragment);
-        Objects.requireNonNull(mapFragment).getMapAsync(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-
-        // Initialize bus stops
-        busStops = new ArrayList<>();
-        busStops.add(new BusStop("Ghandi Square", new LatLng(-26.207079851461764, 28.043346217111473)));
-        busStops.add(new BusStop("Bedford Plaza", new LatLng(-26.177853926086073, 28.134863838086332)));
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_schedules, R.id.navigation_maps, R.id.navigation_mytag)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        myMap = googleMap;
-
-        // Add bus stop markers
-        for (BusStop busStop : busStops) {
-            MarkerOptions markerOptions = new MarkerOptions()
-                    .position(busStop.getLocation())
-                    .title(busStop.getTitle())
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus_pin_icon_foreground));
-            myMap.addMarker(markerOptions);
-        }
-
-        // Move the camera to the first bus stop
-        if (!busStops.isEmpty()) {
-            myMap.moveCamera(CameraUpdateFactory.newLatLng(busStops.get(0).getLocation()));
-        }
-    }
-
-
-    private class BusStop {
-        private String title;
-        private LatLng location;
-
-        public BusStop(String title, LatLng location) {
-            this.title = title;
-            this.location = location;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public LatLng getLocation() {
-            return location;
-        }
-    }
 }
--- app/src/main/java/com/example/metromate01/ui/maps/MapsFragment.java
+++ app/src/main/java/com/example/metromate01/ui/maps/MapsFragment.java
@@ -1,4 +1,4 @@
-package com.example.metromate01.ui.schedules;
+package com.example.metromate01.ui.maps;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.metromate01.databinding.FragmentDashboardBinding;
+import com.example.metromate01.databinding.FragmentMapsBinding;
 
-public class DashboardFragment extends Fragment {
+public class MapsFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentMapsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        MapsViewModel mapsViewModel =
+                new ViewModelProvider(this).get(MapsViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentMapsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textNotifications;
+        mapsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
