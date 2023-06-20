4a118d2e8dc43faee78b2f3d22ff028202bb7aeb
Viveck Ranchod <69462420+ViveckRanchod@users.noreply.github.com>
2023-06-20 15:34:31 +0200

    added drawables

--- app/src/main/java/com/example/metromate01/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/metromate01/ui/notifications/NotificationsFragment.java
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.metromate01.databinding.FragmentNotificationsBinding;
+
 
 public class NotificationsFragment extends Fragment {
 
