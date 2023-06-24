61dbef2a5f2e9bbef63bf71d005fb7804c2761fe
Viveck Ranchod <69462420+ViveckRanchod@users.noreply.github.com>
2023-06-24 15:33:29 +0200

    Created Trip cards frontend

--- app/src/main/java/com/example/metromate01/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/metromate01/ui/home/HomeViewModel.java
@@ -4,7 +4,8 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class
+HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
--- app/src/main/java/com/example/metromate01/ui/maps/MapsFragment.java
+++ app/src/main/java/com/example/metromate01/ui/maps/MapsFragment.java
@@ -24,8 +24,8 @@ public class MapsFragment extends Fragment {
         binding = FragmentMapsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textMaps;
-        mapsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textMaps;
+//        mapsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
