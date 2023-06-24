9d033a362ec347d6ba983838eb6635ee74e23ce0
Juhi Kira <106545129+JKiraX@users.noreply.github.com>
2023-06-24 20:57:57 +0200

    Removed landing page fragment, added landing launch class. Set the landing page to open on launch

---
+++ app/src/main/java/com/example/metromate01/LandingLaunch.java
@@ -0,0 +1,21 @@
+package com.example.metromate01;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LandingLaunch extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+
+
+    }
+
+}
