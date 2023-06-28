5ffcaeef097c4d717b35ae322147178771d0b00e
Juhi Kira <106545129+JKiraX@users.noreply.github.com>
2023-06-28 20:45:42 +0200

    Created a class and navigation fragment for the bus driver side of the app. Navigation for bus driver side complete.

--- app/src/main/java/com/example/metromate01/Driver.java
+++ app/src/main/java/com/example/metromate01/Driver.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -15,127 +16,62 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+
 public class Driver extends AppCompatActivity {
 
-    EditText name, lastname, badgeID, email, password, dob;
-    String path = "driver";
+    EditText name,lastname, badgeID, email,
+            password, dob;
+
+    String path ="driver"; // path set
+    int tagNumber =0;
     Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_registration_page);
-
+        //assign variables to xml elements:
         signUp = findViewById(R.id.btnSignUp);
+        name = findViewById(R.id.edtName);
+        lastname= findViewById(R.id.edtLastName);
+        badgeID=  findViewById(R.id.edtBadgeId);
+        email=  findViewById(R.id.edtEmail);
+        password=  findViewById(R.id.edtPassword);
+        dob=  findViewById(R.id.edtDateOfBirth);
 
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View sendView) {
-                name = findViewById(R.id.edtName);
-                String st_name = name.getText().toString();
+        //send user input to dbase:
+        Database db = new Database();
+        //check input conditions:
 
-                lastname = findViewById(R.id.edtLastName);
-                String st_lastname = lastname.getText().toString();
+        signUp.setOnClickListener(sendView -> {
 
-                badgeID = findViewById(R.id.edtBadgeId);
-                String st_badgeID = badgeID.getText().toString();
+            //get current user inputs:
+            String st_name = name.getText().toString();
+            String st_lastname = lastname.getText().toString();
+            String st_email = email.getText().toString();
+            int in_badgeID = Integer.parseInt(badgeID.getText().toString());
+            String st_password = password.getText().toString();
+            String st_dob = dob.getText().toString();
 
-                email = findViewById(R.id.edtEmail);
-                String st_email = email.getText().toString();
+            //set refValues in the same order of the parameters set in signUpToDatabase in getDatabase:
+            //refer to Database to see purpose of tagNumber= 0
 
-                password = findViewById(R.id.edtPassword);
-                String st_password = password.getText().toString();
-
-                dob = findViewById(R.id.edtDateOfBirth);
-                String st_dob = dob.getText().toString();
-                // convert date again to date format:
-                Date date_dob;
-                SimpleDateFormat sdf_dob = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-                try {
-                    date_dob = sdf_dob.parse(st_dob);
-                } catch (ParseException e) {
-                    throw new RuntimeException(e);
-                }
-
-                Database db = new Database();
+            if(!st_name.isEmpty() && !st_lastname.isEmpty() && !st_email.isEmpty() && !st_password.isEmpty()
+                    && st_password.length()>=8 && !st_dob.isEmpty() && in_badgeID>0)
+            {
+                //pass path we want to send data to:
                 db.setPath(path);
-                assert date_dob != null;
-                db.signUpToDatabase(st_name, st_lastname, st_email, st_password, date_dob, st_badgeID,
-                        null, path);
-
-                Intent intent = new Intent(Driver.this, MainActivity.class);
+                db.signUpToDatabase(st_name, st_lastname, st_email, st_password, st_dob, in_badgeID,
+                        tagNumber, path);
+                Toast.makeText(Driver.this, "You have signed up successfully!", Toast.LENGTH_SHORT).show();
+                // Navigate to BusDriverHomeActivity
+                Intent intent = new Intent(Driver.this, BusDriverActivity.class);
                 startActivity(intent);
-                finish();
+
+            }else {
+                Toast.makeText(Driver.this, "Please ensure are fields are filled & the password is 8 or more characters long", Toast.LENGTH_SHORT).show();
             }
         });
     }
 }
-
-//public class Driver extends AppCompatActivity {
-//
-//    EditText name,lastname, badgeID, email,
-//            password, dob;
-//
-//    String path ="driver";
-//    Button signUp;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_driver_registration_page);
-//
-//
-//    //assign variables to xml elements and convert to string:
-//        signUp = findViewById(R.id.btnSignUp);
-//
-//        name = findViewById(R.id.edtName);
-//        String st_name = name.getText().toString();
-//
-//        lastname= findViewById(R.id.edtLastName);
-//        String st_lastname = lastname.getText().toString();
-//
-//        badgeID=  findViewById(R.id.edtBadgeId);
-//        String st_badgeID = badgeID.getText().toString();
-//
-//        email=  findViewById(R.id.edtEmail);
-//        String st_email = email.getText().toString();
-//
-//        password=  findViewById(R.id.edtPassword);
-//        String st_password = password.getText().toString();
-//
-//        dob=  findViewById(R.id.edtDateOfBirth);
-//        String st_dob = dob.getText().toString();
-//        //convert date again to date format:
-//        Date date_dob;
-//        SimpleDateFormat sdf_dob = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-//        try {
-//            date_dob = sdf_dob.parse(st_dob);
-//        } catch (ParseException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        //send user input to dbase:
-//        signUp.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View sendView){
-//                //pass path we want to send data to:
-//                Database db = new Database();
-//                db.setPath(path);
-//                //set refValues in the same order of the parameters set in signUpToDatabase in getDatabase:
-//                //refer to Database to see purpose of tagNumber= null
-//                assert date_dob != null;
-//                db.signUpToDatabase(st_name, st_lastname, st_email, st_password, date_dob, st_badgeID,
-//                        null, path);
-//
-//                Intent intent = new Intent(Driver.this, MainActivity.class);
-//                startActivity(intent);
-//                finish(); // Optional: If you don't want the user to go back to the sign-up page with the back button
-//
-//            }
-//        });
-//
-//    }
-//}
-
-/* name= (EditText) findViewById(R.id.edtName);
-        String st_name = name.getText().toString();*/
