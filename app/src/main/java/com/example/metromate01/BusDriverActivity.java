e545a546e388e30c1c76eb65be33f21efef4b8b6
Juhi Kira <106545129+JKiraX@users.noreply.github.com>
2023-06-28 20:45:42 +0200

    Created a class and navigation fragment for the bus driver side of the app. Navigation for bus driver side complete.

---
+++ app/src/main/java/com/example/metromate01/BusDriverActivity.java
@@ -0,0 +1,35 @@
+package com.example.metromate01;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.metromate01.databinding.ActivityBusDriverBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BusDriverActivity extends AppCompatActivity {
+    private ActivityBusDriverBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityBusDriverBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view1);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_bus_home, R.id.navigation_bus_schedules, R.id.navigation_bus_maps)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_bus_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView1, navController);
+    }
+}
