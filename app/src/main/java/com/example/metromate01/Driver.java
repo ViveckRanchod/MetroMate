e00baa553b896588a08111a63eeec370a3ac9e67
Juhi Kira <106545129+JKiraX@users.noreply.github.com>
2023-06-29 11:35:33 +0200

    Shared Preferences code for keeping the session going after registration is implemented

--- app/src/main/java/com/example/metromate01/Driver.java
+++ app/src/main/java/com/example/metromate01/Driver.java
@@ -3,6 +3,7 @@ package com.example.metromate01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,33 +21,25 @@ import java.util.Locale;
 
 public class Driver extends AppCompatActivity {
 
-    EditText name,lastname, badgeID, email,
-            password, dob;
-
-    String path ="driver"; // path set
-    int tagNumber =0;
+    EditText name, lastname, badgeID, email, password, dob;
+    String path = "driver";
+    int tagNumber = 0;
     Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_registration_page);
-        //assign variables to xml elements:
+
         signUp = findViewById(R.id.btnSignUp);
         name = findViewById(R.id.edtName);
-        lastname= findViewById(R.id.edtLastName);
-        badgeID=  findViewById(R.id.edtBadgeId);
-        email=  findViewById(R.id.edtEmail);
-        password=  findViewById(R.id.edtPassword);
-        dob=  findViewById(R.id.edtDateOfBirth);
+        lastname = findViewById(R.id.edtLastName);
+        badgeID = findViewById(R.id.edtBadgeId);
+        email = findViewById(R.id.edtEmail);
+        password = findViewById(R.id.edtPassword);
+        dob = findViewById(R.id.edtDateOfBirth);
 
-        //send user input to dbase:
-        Database db = new Database();
-        //check input conditions:
-
-        signUp.setOnClickListener(sendView -> {
-
-            //get current user inputs:
+        signUp.setOnClickListener(view -> {
             String st_name = name.getText().toString();
             String st_lastname = lastname.getText().toString();
             String st_email = email.getText().toString();
@@ -54,23 +47,23 @@ public class Driver extends AppCompatActivity {
             String st_password = password.getText().toString();
             String st_dob = dob.getText().toString();
 
-            //set refValues in the same order of the parameters set in signUpToDatabase in getDatabase:
-            //refer to Database to see purpose of tagNumber= 0
-
-            if(!st_name.isEmpty() && !st_lastname.isEmpty() && !st_email.isEmpty() && !st_password.isEmpty()
-                    && st_password.length()>=8 && !st_dob.isEmpty() && in_badgeID>0)
-            {
-                //pass path we want to send data to:
+            if (!st_name.isEmpty() && !st_lastname.isEmpty() && !st_email.isEmpty() && !st_password.isEmpty()
+                    && st_password.length() >= 8 && !st_dob.isEmpty() && in_badgeID > 0) {
+                Database db = new Database();
                 db.setPath(path);
-                db.signUpToDatabase(st_name, st_lastname, st_email, st_password, st_dob, in_badgeID,
-                        tagNumber, path);
+                db.signUpToDatabase(st_name, st_lastname, st_email, st_password, st_dob, in_badgeID, tagNumber, path);
                 Toast.makeText(Driver.this, "You have signed up successfully!", Toast.LENGTH_SHORT).show();
-                // Navigate to BusDriverHomeActivity
+
+                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("isLoggedIn", true);
+                editor.apply();
+
                 Intent intent = new Intent(Driver.this, BusDriverActivity.class);
                 startActivity(intent);
-
-            }else {
-                Toast.makeText(Driver.this, "Please ensure are fields are filled & the password is 8 or more characters long", Toast.LENGTH_SHORT).show();
+                finish(); // Optional: Finish the current activity to prevent going back to the registration page
+            } else {
+                Toast.makeText(Driver.this, "Please ensure all fields are filled and the password is 8 or more characters long", Toast.LENGTH_SHORT).show();
             }
         });
     }
