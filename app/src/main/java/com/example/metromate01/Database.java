caebd636410ddf345def5841eb94409b8d58eb6e
Musa <musamichelledube@gmail.com>
2023-06-30 14:00:50 +0200

    removed warnings

--- app/src/main/java/com/example/metromate01/Database.java
+++ app/src/main/java/com/example/metromate01/Database.java
@@ -1,17 +1,10 @@
 package com.example.metromate01;
 
-import androidx.annotation.NonNull;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
+
 
 public class Database {
     //reference database:
