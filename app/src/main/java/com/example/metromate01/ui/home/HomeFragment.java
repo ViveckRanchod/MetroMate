8c89e186458602b0ba878cb77801f91fa22e0ee5
Juhi Kira <106545129+JKiraX@users.noreply.github.com>
2023-07-01 13:11:44 +0200

    User profile image imports. User profile page and log out function added.

--- app/src/main/java/com/example/metromate01/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/metromate01/ui/home/HomeFragment.java
@@ -1,31 +1,46 @@
 package com.example.metromate01.ui.home;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.metromate01.R;
+import com.example.metromate01.UserProfileCommuterActivity;
 import com.example.metromate01.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private ImageButton profileImageButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                ViewGroup container, Bundle savedInstanceState) {
-            HomeViewModel homeViewModel =
-                    new ViewModelProvider(this).get(HomeViewModel.class);
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        profileImageButton = root.findViewById(R.id.profileImageButton);
+        profileImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the user profile activity
+                Intent intent = new Intent(getActivity(), UserProfileCommuterActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return root;
     }
 
