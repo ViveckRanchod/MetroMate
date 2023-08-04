cc4c93262d5535124530d46d2d39d5da2eb7c5ad
Musa <musamichelledube@gmail.com>
2023-08-04 19:46:29 +0200

    added source code for schedule page functionality

--- app/src/main/java/com/example/metromate01/ui/schedules/SchedulesFragment.java
+++ app/src/main/java/com/example/metromate01/ui/schedules/SchedulesFragment.java
@@ -4,18 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.metromate01.R;
 import com.example.metromate01.databinding.FragmentActivitySchedulePageBinding;
+import com.example.metromate01.ui.schedules.SchedulesViewModel;
 
-
-public class SchedulesFragment extends Fragment {
+public class SchedulesFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private FragmentActivitySchedulePageBinding binding;
+    Spinner schedule_droplist;
+    ImageView schedule_img;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,9 +32,39 @@ public class SchedulesFragment extends Fragment {
         binding = FragmentActivitySchedulePageBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        schedulesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        schedule_droplist = root.findViewById(R.id.schedule_droplist);
+        ArrayAdapter<CharSequence> adaptArr = ArrayAdapter.createFromResource(requireContext(), R.array.areas_array, android.R.layout.simple_spinner_item);
+        adaptArr.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        schedule_droplist.setAdapter(adaptArr);
+        schedule_droplist.setSelection(0); //set default display
+        schedule_droplist.setOnItemSelectedListener(this);
+
+        schedule_img = root.findViewById(R.id.schedule_img);
+
         return root;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        int schedule_selected_id = schedule_droplist.getSelectedItemPosition();
+        switch (schedule_selected_id) {
+            case 0:
+                schedule_img.setImageResource(R.drawable.bus_background);
+                break;
+            case 1:
+                schedule_img.setImageResource(R.drawable.oakdene_schedule);
+                break;
+            case 2:
+                schedule_img.setImageResource(R.drawable.bedford_schedule);
+                break;
+            default:
+                Toast.makeText(requireContext(), "ERROR: unable to display schedule", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        schedule_img.setImageResource(R.drawable.bus_background);
     }
 
     @Override
